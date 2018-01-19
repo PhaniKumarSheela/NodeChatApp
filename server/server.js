c41cfd72087f6feaf91fd6f41f3b6dfdab50d31e
PhaniKumarSheela <34272762+PhaniKumarSheela@users.noreply.github.com>
2018-01-18 20:20:56 -0500

    Bug Fix

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express = require('express');
 
 const publicpath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
+var app = express();
 
-app = express();
 app.use(express.static(publicpath));
 
 app.listen(3000,()=> {
