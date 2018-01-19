af0d3acadbc291a31a5a5daf06a3fa7179b2dc5e
PhaniKumarSheela <34272762+PhaniKumarSheela@users.noreply.github.com>
2018-01-19 00:16:57 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -12,13 +12,33 @@ var io = socketIO(server);
 io.on('connection', (socket) =>{
     console.log('New user connected');
 
+    //socket.emit from Admin text Welcome to the chat App
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to the Chat App'
+    });
+
+    //socket.broadcast.emit from Admin text New user joined
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage',(message) => {
         console.log('createMessage',message);
+
         io.emit('newMessage',{
             from:message.from,
             text:message.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage',{
+        //     from:message.from,
+        //     text:message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.emit('newMessage',{
